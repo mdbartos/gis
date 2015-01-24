d58bb1e5bdf0c70072801f2d4a70821aa7181e25
Matthew Bartos <matthew.d.bartos@gmail.com>
2015-01-23 22:54:32 -0700

    Update KDTree_join_looped.py

--- KDTree_join_looped.py
+++ KDTree_join_looped.py
@@ -10,30 +10,56 @@ from matplotlib import path
 import os
 from datetime import datetime
 import sys
+import pysal
 
 class quick_spatial_join():
-    def __init__(self, shp1, shp2, convert_crs=0, memsize=240000000):
+    def __init__(self, shp1, shp2, convert_crs=0, memsize=240000000, man_input=None, autorun=True, tiebreak=True):
         print 'START: %s' % (str(datetime.now()))
         print 'loading files...'        
-        self.shapes = {
-                        'shp1' : {
-                            'file' : fiona.open(shp1, 'r')
-                            },
-                
-                        'shp2': {
-                            'file' : fiona.open(shp2, 'r')
-                            }
-                }
         
         print 'getting geometry type info...'
 
-        self.shapes['shp1'].update({'crs': self.shapes['shp1']['file'].crs})
-        self.shapes['shp1'].update({'types': self.geom_types(self.shapes['shp1']['file']).dropna()})
-        self.shapes['shp2'].update({'crs': self.shapes['shp2']['file'].crs})
-        self.shapes['shp2'].update({'types': self.geom_types(self.shapes['shp2']['file']).dropna()})
+        self.tiebreak = tiebreak
 
-        self.shapes['shp2'].update({'shp' : self.homogenize_inputs('shp2', range(len(self.shapes['shp2']['file'])))})
-        self.shapes['shp2'].update({'poly' : self.poly_return('shp2')})
+        if man_input==None:
+
+            self.shapes = {
+                            'shp1' : {
+                                'file' : fiona.open(shp1, 'r')
+                                },
+                    
+                            'shp2': {
+                                'file' : fiona.open(shp2, 'r')
+                                }
+                    }
+
+            self.shapes['shp1'].update({'crs': self.shapes['shp1']['file'].crs})
+            self.shapes['shp1'].update({'types': self.geom_types(self.shapes['shp1']['file']).dropna()})
+            self.shapes['shp2'].update({'crs': self.shapes['shp2']['file'].crs})
+            self.shapes['shp2'].update({'types': self.geom_types(self.shapes['shp2']['file']).dropna()})
+    
+            self.shapes['shp2'].update({'shp' : self.homogenize_inputs('shp2', range(len(self.shapes['shp2']['file'])))})
+            self.shapes['shp2'].update({'poly' : self.poly_return('shp2')})
+
+        else:
+
+            self.shapes = {
+                            'shp1' : {
+                                'file' : fiona.open(shp1, 'r')
+                                },
+                    
+                            'shp2': {
+                                'file' : None
+                                }
+                    }
+
+            self.shapes['shp1'].update({'crs': self.shapes['shp1']['file'].crs})
+            self.shapes['shp1'].update({'types': self.geom_types(self.shapes['shp1']['file']).dropna()})
+            self.shapes['shp2'].update({'crs': man_input['crs']})
+	    self.shapes['shp2'].update({'types': man_input['types']})
+            self.shapes['shp2'].update({'shp': man_input['shp']})
+            self.shapes['shp2'].update({'poly' : self.poly_return('shp2')})
+
         self.make_kdtree()
 
         self.memratio = 1 + os.path.getsize(shp1)/memsize
@@ -43,24 +69,30 @@ class quick_spatial_join():
         chunk_ct = 0
 
         self.matches = {}
-
-        for chunk_n in self.chunks:
-
-            print 'Loop %s of %s...' % (chunk_ct+1, self.memratio)
-            if self.shapes['shp1']['crs'] != self.shapes['shp2']['crs']:
-                self.shapes['shp1'].update({'shp' : self.convert_crs('shp1', self.shapes['shp1']['crs'], self.shapes['shp2']['crs'], chunk_n)})
-            else:
-                self.shapes['shp1'].update({'shp' : self.homogenize_inputs('shp1', chunk_n)})
-            
-            self.shapes['shp1'].update({'poly' : self.poly_return('shp1')})
-
-            self.shp1_centroids, self.treequery = self.query_tree()
-
-            self.matches[chunk_ct] = self.check_matches()
-
-            chunk_ct = chunk_ct + 1
-
-        print 'END: %s' % (str(datetime.now()))
+        
+	if autorun==True:
+            for chunk_n in self.chunks:
+    
+                print 'Loop %s of %s...' % (chunk_ct+1, self.memratio)
+                if self.shapes['shp1']['crs'] != self.shapes['shp2']['crs']:
+                    self.shapes['shp1'].update({'shp' : self.convert_crs('shp1', self.shapes['shp1']['crs'], self.shapes['shp2']['crs'], chunk_n)})
+                else:
+                    self.shapes['shp1'].update({'shp' : self.homogenize_inputs('shp1', chunk_n)})
+                
+                self.shapes['shp1'].update({'poly' : self.poly_return('shp1')})
+    
+                self.shp1_centroids, self.treequery = self.query_tree()
+    
+                self.matches[chunk_ct] = self.check_matches()
+    
+                chunk_ct = chunk_ct + 1
+    
+            if len(self.matches)>1:
+    		self.matches = pd.concat([i for i in self.matches.values()])
+    	    elif len(self.matches)==1:
+    		self.matches = self.matches[0]
+    
+            print 'END: %s' % (str(datetime.now()))
 
 
     def file_chunks(self, l, n):
@@ -200,7 +232,6 @@ class quick_spatial_join():
                 return np.nan
             
         return s.apply(return_coords)
-      
     
     def handle_topo_err(self, k):
         if k.is_valid:
@@ -211,26 +242,46 @@ class quick_spatial_join():
     def handle_empty(self, k):
         if k.is_empty:
             return np.nan
+        elif type(k) != geometry.polygon.Polygon:
+            return np.nan
         else:
             return k
 
+    def try_union(self, k):
+        try:
+    	    return ops.cascaded_union(k)
+        except:
+            try:
+    	        u = k[0]
+    	        for z in range(len(k))[1:]:
+    	            u = u.union(k[z])
+    	        return u
+            except:
+                return geometry.Polygon(np.vstack(pd.Series(k).apply(lambda x: x.boundary.coords).apply(np.array))).convex_hull
+
     def poly_return(self, shp):
         print 'creating polygons for %s...' % (shp)
-        print 'making poly df'
         poly_df = pd.Series(index=self.shapes[shp]['shp'].index)
 
         geomtypes = self.shapes[shp]['types'].loc[poly_df.index]
         
 #        print 'making p'
-        p = self.shapes[shp]['shp'].loc[geomtypes=='Polygon'].apply(lambda x: geometry.Polygon(x))#.apply(self.handle_empty) 
-#        print 'setting polydf with p'
-        poly_df.loc[p.index] = p.copy()
+        if (geomtypes=='Polygon').any():
+            p = self.shapes[shp]['shp'].loc[geomtypes=='Polygon'].apply(lambda x: geometry.Polygon(x))#.apply(self.handle_empty) 
+    #        print 'setting polydf with p'
+            poly_df.loc[p.index] = p.copy()
         
 #        print 'making mp'
-        mp = self.shapes[shp]['shp'].loc[geomtypes == 'MultiPolygon'].apply(lambda x: (pd.Series(list(x)))).stack().apply(geometry.Polygon).apply(self.handle_topo_err).apply(self.handle_empty).reset_index().groupby('level_0').apply(lambda x: ops.cascaded_union(list(x[0])))
-        
-#        print 'setting poly df with mp'
-        poly_df.loc[mp.index] = mp.copy()
+        if (geomtypes=='MultiPolygon').any():
+            mp = self.shapes[shp]['shp'].loc[geomtypes == 'MultiPolygon'].apply(lambda x: (pd.Series(list(x)))).stack().apply(geometry.Polygon)
+	    
+	    if mp.apply(lambda x: not x.is_valid).any():
+	        mp = mp.apply(self.handle_topo_err).apply(self.handle_empty).dropna()
+		
+	    mp = mp.reset_index().groupby('level_0').apply(lambda x: list(x[0])).apply(self.try_union)
+            
+    #        print 'setting poly df with mp'
+            poly_df.loc[mp.index] = mp.copy()
 
 #        print 'making nullgeom'
         nullgeom = poly_df[poly_df.isnull()].index
@@ -316,11 +367,22 @@ class quick_spatial_join():
 
         nullidx = match_d[match_d.isnull().all(axis=1)].index
 
-        poly = self.shapes['shp2']['shp'].loc[self.shapes['shp2']['types']=='Polygon']
-        mpoly = self.shapes['shp2']['shp'].loc[self.shapes['shp2']['types']== 'MultiPolygon'].apply(lambda x: (pd.Series(list(x)))).stack()
-        p_midx = pd.MultiIndex.from_arrays([np.array(poly.index), np.zeros(len(poly.index))])
-        poly.index = p_midx
-        poly_all = pd.concat([poly, mpoly])
+	if (self.shapes['shp2']['types']=='Polygon').any():
+            poly = self.shapes['shp2']['shp'].loc[self.shapes['shp2']['types']=='Polygon']
+
+	if (self.shapes['shp2']['types']=='MultiPolygon').any():
+            mpoly = self.shapes['shp2']['shp'].loc[self.shapes['shp2']['types']== 'MultiPolygon'].apply(lambda x: (pd.Series(list(x)))).stack()
+        
+	if (self.shapes['shp2']['types']=='Polygon').any() and (self.shapes['shp2']['types']=='MultiPolygon').any():  
+	    p_midx = pd.MultiIndex.from_arrays([np.array(poly.index), np.zeros(len(poly.index))])
+            poly.index = p_midx
+            poly_all = pd.concat([poly, mpoly])
+	elif (self.shapes['shp2']['types']=='Polygon').any() and not (self.shapes['shp2']['types']=='MultiPolygon').any():
+	        p_midx = pd.MultiIndex.from_arrays([np.array(poly.index), np.zeros(len(poly.index))])
+                poly.index = p_midx
+		poly_all = poly
+	elif (self.shapes['shp2']['types']=='MultiPolygon').any() and not (self.shapes['shp2']['types']=='Polygon').any():
+		poly_all = mpoly
 
         polypath = poly_all.apply(lambda x: path.Path(x))
 
@@ -336,15 +398,14 @@ class quick_spatial_join():
 
             nontree_matches = pd.DataFrame(poly_result.index.get_level_values(0), index=poly_result.values)
 
+	    if self.tiebreak==True:
+                n_ties = nontree_matches.reset_index()
+                n_ties['c_1'] = n_ties['index'].apply(lambda x: self.shapes['shp1']['poly'][x].centroid)
+                n_ties['c_2'] = n_ties[0].apply(lambda x: self.shapes['shp2']['poly'][x].centroid)
+                n_ties['d'] = n_ties.apply(lambda x: x['c_1'].distance(x['c_2']), axis=1)
+                nontree_matches = pd.DataFrame(n_ties.groupby('index')['d'].agg(lambda x: n_ties[0].loc[x.idxmin()])).rename(columns={'d':0})  
+
             return pd.concat([match_d, nontree_matches]).sort_index().dropna(how='all')
             
         else:
             return match_d.sort_index().dropna(how='all')
-
-
-
-la = '/home/tabris/Desktop/Vulnerability Files/parcels_AIN_2.shp'
-latracts = '/home/tabris/Desktop/Vulnerability Files/tl_2010_06037_tract10.shp'
-
-b = quick_spatial_join(la, latracts, memsize=400000000)
-#b = quick_spatial_join(la, latracts)
