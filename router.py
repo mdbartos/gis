6dd0bfb9a42d75108313bd2e046411a9b3283f90
Matthew Bartos <matthew.d.bartos@gmail.com>
2015-04-18 03:24:59 -0700

    Update router.py

--- router.py
+++ router.py
@@ -1,56 +1,75 @@
 import numpy as np
 
-g = np.random.uniform(200,210,100).reshape(10,10)
-#np.vstack(np.dstack(np.indices(g.shape)))
+g = np.random.uniform(200,210,16).reshape(4,4)
 
-def build_args(arr):
+def d8_flowdir(g, nodata=0):
     
     idx = np.indices(g.shape)
     
     # ALL GOING CLOCKWISE
     #CORNERS
     c = {
-    'nw' : {'k' : idx[:,0,0], 'v' : [[1,1,0], [0,1,1]]}
-    'ne' : {'k' : idx[:,0,-1], 'v' : [[-1,-2,-2], [1,1,0]]}
-    'sw' : {'k' : idx[:,-1,0], 'v' : [[1,1,0], [-2,-2,-1]]}
-    'se' : {'k' : idx[:,-1,-1], 'v' : [[-1,-2,-2], [-1,-2,-2]]}
+    'nw' : {'k' : tuple(idx[:,0,0]), 'v' : [[0,1,1], [1,1,0]], 'pad': 3},
+    'ne' : {'k' : tuple(idx[:,0,-1]), 'v' : [[1,1,0], [-1,-2,-2]], 'pad': 5},
+    'sw' : {'k' : tuple(idx[:,-1,0]), 'v' : [[-2,-2,-1], [0,1,1]], 'pad': 1},
+    'se' : {'k' : tuple(idx[:,-1,-1]), 'v' : [[-1,-2,-2], [-2,-2,-1]], 'pad': 7}
     }
 
     #edges
-    edges = {
-    'n' : idx[:,0,1:-1],
-    'w' : idx[:,1:-1,0],
-    'e' : idx[:,1:-1,-1],
-    's' : idx[:,-1,1:-1],
+    edge = {
+    'n' : {'k' : tuple(idx[:,0,1:-1]), 'pad' : np.array([3,4,5,6,7])},
+    'w' : {'k' : tuple(idx[:,1:-1,0]), 'pad' : np.array([1,2,3,4,5])},
+    'e' : {'k' : tuple(idx[:,1:-1,-1]), 'pad' : np.array([1,5,6,7,8])},
+    's' : {'k' : tuple(idx[:,-1,1:-1]), 'pad' : np.array([1,2,3,7,8])}
     }
 
     #body
     body = idx[:, 1:-1, 1:-1]
 
-    outmap = np.empty(g.shape)
+    outmap = np.zeros(g.shape, dtype=np.int8)
 
-def select_corners(k):
-    pass
 
-def select_surround(i, j):
-    return [i+0, i+1, i+1, i+1, i+0, i-1, i-1, i-1], [j+1, j+1, j+0, j-1, j-1, j-1, j+0, j+1]
+    def select_surround(i, j):
+        return ([i-1, i-1, i+0, i+1, i+1, i+1, i+0, i-1],
+               [j+0, j+1, j+1, j+1, j+0, j-1, j-1, j-1])
 
-f = np.vectorize(select_surround)
 
-# ONE-BY-ONE
-for i, j in np.nditer([body[0], body[1]]):
-    dif = (g[i,j] - g[f(i,j)])
-    if (dif > 0).any():
-        outmap[i,j] = dif.argmax() + 1
-    else:
-        outmap[i,j] = 0
+    def select_edge_sur(k):
+        i,j = edge[k]['k']
+        if k == 'n':
+            return [i+0, i+1, i+1, i+1, i+0], [j+1, j+1, j+0, j-1, j-1]
+        elif k =='e':
+            return [i-1, i+1, i+1, i+0, i-1], [j+0, j+0, j-1, j-1, j-1]
+        elif k =='s':
+            return [i-1, i-1, i+0, i+0, i-1], [j+0, j+1, j+1, j-1, j-1]
+        elif k == 'w':
+            return [i-1, i-1, i+0, i+1, i+1], [j+0, j+1, j+1, j+1, j+0]
 
-# ROW-BY-ROW
-for i, j in np.nditer([body[0], body[1]], flags=['external_loop']):
-    dat = g[i,j]
-    sur = g[select_surround(i,j)]
-    a = ((dat - sur) > 0).any(axis=0)
-    b = np.argmax((dat - sur), axis=0) + 1
-    c = 0
-    outmap2[i,j] = np.where(a,b,c)
+    # FILL CORNERS
+    for i in c.keys():
+        dat = g[c[i]['k']]
+        sur = g[c[i]['v']]
+        if ((dat - sur) > 0).any():
+            outmap[c[i]['k']] = np.argmax(dat - sur) + c[i]['pad']
+        else:
+            outmap[c[i]['k']] = 0
 
+    # FILL BODY
+    for i, j in np.nditer(tuple(body), flags=['external_loop']):
+        dat = g[i,j]
+        sur = g[select_surround(i,j)]
+        a = ((dat - sur) > 0).any(axis=0)
+        b = np.argmax((dat - sur), axis=0) + 1
+        c = 0
+        outmap[i,j] = np.where(a,b,c)
+
+    #FILL EDGES
+    for x in edge.keys():
+        dat = g[edge[x]['k']]
+        sur = g[select_edge_sur(x)]
+        a = ((dat - sur) > 0).any(axis=0)
+        b = edge[x]['pad'][np.argmax((dat - sur), axis=0)]
+        c = 0
+        outmap[edge[x]['k']] = np.where(a,b,c)
+
+    return outmap
